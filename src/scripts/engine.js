8f795bdda1078489567c7ee6dc4a36132c1aa089
Soren <danilo-258@hotmail.com>
2024-10-25 15:44:46 -0300

    feat(js): adiciona lógica de movimentação e controle de velocidade do inimigo em engine.js
    - Atualizada a constante `state` com novas propriedades `timerId` e `gameVelocity` para controlar a velocidade e o estado do jogo.
    - Adicionada função `randomSquare()` para selecionar e atualizar a posição do inimigo aleatoriamente.
    - Implementada função `moveEnemy()` para mover o inimigo em intervalos controlados por `gameVelocity`.
    - Estrutura inicial de `addListenerHitBox()` para futuras adições de escuta de eventos em `squares`.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,10 +1,38 @@
 const state = {
   view:{
-    squares: document.quarySelectorAll(".square"),
+    squares: document.querySelectorAll(".square"),
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
   },
-  values:{},
+  values:{
+    timerId: null,
+    gameVelocity: 1000
+  },
 };
 
+function randomSquare(){
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  state.view.squares.forEach((square)=>{
+    square.classList.remove("enemy");
+  });
+
+  const randomNumber = Math.floor(Math.random() * 9);
+  const randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+}
+
+function moveEnemy(){
+  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function addListenerHitBox(){
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  state.view.squares.forEach((square) => {})
+}
+
+function main(){
+  moveEnemy();
+}
+
+main();
