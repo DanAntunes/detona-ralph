1c940bb8e25e3b49758e5a1dbe0f7cba0d370733
Soren <danilo-258@hotmail.com>
2024-10-25 22:59:37 -0300

    feat(js): adiciona cronômetro de contagem regressiva e lógica de pontuação em engine.js
    - Atualizada a constante `state` com novos valores para `hitPosition`, `result` e `currentTime` para rastrear a posição do inimigo, pontuação do jogador e tempo restante.
    - Adicionada função `countDown()` para controlar a contagem regressiva e finalizar o jogo quando o tempo se esgota.
    - Adicionada função `playSound()` para reproduzir som ao acertar o inimigo.
    - Função `randomSquare()` agora atualiza `hitPosition` para controle de pontuação.
    - Função `addListenerHitBox()` adicionada para incrementar a pontuação e reproduzir som ao clicar na posição correta.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,10 +6,33 @@ const state = {
     score: document.querySelector("#score"),
   },
   values:{
-    timerId: null,
-    gameVelocity: 1000
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
   },
+  actions:{
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
+  }
 };
+
+function countDown(){
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+  if (state.values.currentTime <= 0){
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+    // biome-ignore lint/style/useTemplate: <explanation>
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+  }
+}
+
+function playSound(){
+  const audio = new Audio("./src/sounds/src_audios_hit.m4a");
+  audio.volume = 0.2;
+  audio.play();
+}
 
 function randomSquare(){
   // biome-ignore lint/complexity/noForEach: <explanation>
@@ -20,19 +43,25 @@ function randomSquare(){
   const randomNumber = Math.floor(Math.random() * 9);
   const randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
-}
-
-function moveEnemy(){
-  state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+  state.values.hitPosition = randomSquare.id;
 }
 
 function addListenerHitBox(){
   // biome-ignore lint/complexity/noForEach: <explanation>
-  state.view.squares.forEach((square) => {})
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown",() => {
+      if(square.id === state.values.hitPosition){
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound();
+      }
+    });
+  });
 }
 
 function main(){
-  moveEnemy();
+  addListenerHitBox();
 }
 
 main();
